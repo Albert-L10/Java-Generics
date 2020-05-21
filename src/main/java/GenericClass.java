3ba055c07247a9c5370eafe8d7b0e34f4cded0fe
Albert-L10 <59960489+Albert-L10@users.noreply.github.com>
2020-05-22 00:47:32 +0200

    Add files via upload
    Second commit

--- src/main/java/GenericClass.java
+++ src/main/java/GenericClass.java
@@ -1,44 +1,21 @@
-public class GenericClass<T>{
+public class GenericClass<T> {
     public T elements;
 
-    public void setElements(T elements){
+    public void setElements(T elements) {
 
         this.elements = elements;
     }
-    public T getElements(){
+
+    public T getElements() {
 
         return elements;
     }
 
-
-    public  void printArrayContent(T[] array)
+    public void printArrayContent(T[] array)
     {
         for (int i = 0 ; i < array.length;i++){
             System.out.println(array[i]);
         }
-
-
-    }
-
-    public static void main(String [] args)
-    {
-        Integer[] numbers={324,6,7900};
-        String[] strLine={"Albert","Is","smart"};
-
-        GenericClass<Integer> integerInstance =new GenericClass<>();
-        integerInstance.setElements(2000);
-        System.out.println(integerInstance.getElements());
-
-        integerInstance.printArrayContent(numbers);
-
-        System.out.println();
-
-        GenericClass<String> stringInstance=new GenericClass<>();
-        stringInstance.setElements("Test");
-        System.out.println(stringInstance.getElements());
-
-        stringInstance.printArrayContent(strLine);
-
 
 
     }
